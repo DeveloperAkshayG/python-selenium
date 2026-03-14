debb81048009269a9654cf80542a0ada078e8748
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2026-03-14 12:04:18 +0530

    second webpage opening using selenium python

---
+++ secondurlopen.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+
+# open browser
+driver=webdriver.Chrome()
+time.sleep(5)
+
+#open webpage
+driver.get("https://www.chinchpoklichachintamani.com")
+time.sleep(5)
