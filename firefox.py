8ccb42f9b138c8c5bc0f8277e9843f95bd7a5203
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2026-05-07 01:40:58 +0530

    selenium testing with firefox browser

---
+++ firefox.py
@@ -0,0 +1,31 @@
+import time
+
+from selenium import webdriver
+
+# instantiate driver for edge
+driver=webdriver.Firefox()
+
+# OPEN url
+driver.get("https://www.cricbuzz.com")
+driver.maximize_window()
+time.sleep(5)
+
+# open another url
+driver.get("https://www.google.com")
+driver.maximize_window()
+time.sleep(5)
+
+# navigate back
+driver.back()
+time.sleep(5)
+
+# referesh  browser
+driver.refresh()
+time.sleep(5)
+
+# navigate forward
+driver.forward()
+time.sleep(5)
+
+# close browser
+driver.quit()
